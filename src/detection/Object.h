4054948329db58b1fadf08cf12cb39eb069454a2
Michael Pohl <mpohl100@googlemail.com>
2024-01-20 15:05:18 +0000

    made slices a member of object

--- src/detection/Object.h
+++ src/detection/Object.h
@@ -1,14 +1,25 @@
 #pragma once
 
+#include "Slices.h"
+
 #include "matrix/Matrix.h"
 
 #include "Rectangle.h"
 
+#include <memory>
 #include <vector>
 
 namespace od {
 
-struct Object {};
+struct Object {
+  Object() = default;
+  Object(const Object &) = default;
+  Object(Object &&) = default;
+  Object &operator=(const Object &) = default;
+  Object &operator=(Object &&) = default;
+  Object(const Slices& slices) : slices{slices} {}
+  Slices slices;
+};
 
 struct ObjectsPerRectangle {
 
