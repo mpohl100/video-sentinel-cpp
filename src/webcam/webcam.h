ce1f515f7432a1fdf57fef2b731e87ebbd5d0b15
Michael Pohl <mpohl100@googlemail.com>
2024-01-21 15:14:25 +0100

    first draft of object merging

--- src/webcam/webcam.h
+++ src/webcam/webcam.h
@@ -1,6 +1,6 @@
 #include "detection/Detection.h"
-#include "detection/Slices.h"
 #include "detection/Object.h"
+#include "detection/Slices.h"
 
 #include "par/parallel.h"
 
@@ -34,7 +34,7 @@ struct FrameData {
   cv::Mat gradient;
   cv::Mat smoothed_contours_mat;
   cv::Mat smoothed_gradient_mat;
-  od::AllObjects all_objects;
+  od::ObjectsPerRectangle all_objects;
   od::AllRectangles all_rectangles;
 
   FrameData() = default;
@@ -56,8 +56,15 @@ FrameData process_frame_quadview(const cv::Mat &imgOriginal,
                                  int gradient_threshold, int nb_splits = 2);
 
 FrameData process_frame_merged(const cv::Mat &imgOriginal,
-                                 const od::Rectangle &rectangle,
-                                 par::Executor &executor, int rings,
-                                 int gradient_threshold, int nb_pixels_per_tile = 100);
+                               const od::Rectangle &rectangle,
+                               par::Executor &executor, int rings,
+                               int gradient_threshold,
+                               int nb_pixels_per_tile = 100);
+
+FrameData process_frame_merge_objects(const cv::Mat &imgOriginal,
+                                      const od::Rectangle &rectangle,
+                                      par::Executor &executor, int rings,
+                                      int gradient_threshold,
+                                      int nb_pixels_per_tile = 100);
 
 } // namespace webcam
