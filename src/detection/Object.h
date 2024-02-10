f0ba20eecf9d24c0fd66d894bce1b16ac99618ed
Michael Pohl <mpohl100@googlemail.com>
2024-02-10 18:46:22 +0000

    more general function of append

--- src/detection/Object.h
+++ src/detection/Object.h
@@ -23,19 +23,24 @@ struct Object {
 
   bool try_merge_right(Object &other) {
     bool do_log = false;
-    if(get_bounding_box().to_math2d_rectangle().area() > 100 && other.get_bounding_box().to_math2d_rectangle().area() > 100){
+    if (get_bounding_box().to_math2d_rectangle().area() > 100 &&
+        other.get_bounding_box().to_math2d_rectangle().area() > 100) {
       do_log = true;
       std::cout << "Encountered two bigger rectangles";
     }
     if (slices.touching_right(other.slices)) {
       slices.merge_right(other.slices);
-      if(do_log){
-        std::cout << "Merged right: " << slices.to_rectangle().to_string() << " with " << other.slices.to_rectangle().to_string() << std::endl;
+      if (do_log) {
+        std::cout << "Merged right: " << slices.to_rectangle().to_string()
+                  << " with " << other.slices.to_rectangle().to_string()
+                  << std::endl;
       }
       return true;
     }
-    if(do_log){
-      std::cout << "Did not merge right: " << slices.to_rectangle().to_string() << " with " << other.slices.to_rectangle().to_string() << std::endl;
+    if (do_log) {
+      std::cout << "Did not merge right: " << slices.to_rectangle().to_string()
+                << " with " << other.slices.to_rectangle().to_string()
+                << std::endl;
     }
     return false;
   }
@@ -54,19 +59,30 @@ struct Object {
 };
 
 struct ObjectsPerRectangle {
-  const std::vector<std::shared_ptr<Object>> &get_objects() const { return objects; }
-  const std::vector<std::shared_ptr<Object>> &get_objects_touching_right() const { return objects_touching_right; }
-  const std::vector<std::shared_ptr<Object>> &get_objects_touching_left() const { return objects_touching_left; }
-  const std::vector<std::shared_ptr<Object>> &get_objects_touching_down() const { return objects_touching_down; }
-  const std::vector<std::shared_ptr<Object>> &get_objects_touching_up() const { return objects_touching_up; }
+  const std::vector<std::shared_ptr<Object>> &get_objects() const {
+    return objects;
+  }
+  const std::vector<std::shared_ptr<Object>> &
+  get_objects_touching_right() const {
+    return objects_touching_right;
+  }
+  const std::vector<std::shared_ptr<Object>> &
+  get_objects_touching_left() const {
+    return objects_touching_left;
+  }
+  const std::vector<std::shared_ptr<Object>> &
+  get_objects_touching_down() const {
+    return objects_touching_down;
+  }
+  const std::vector<std::shared_ptr<Object>> &get_objects_touching_up() const {
+    return objects_touching_up;
+  }
 
   void set_rectangle(const Rectangle &rectangle) {
     this->rectangle = rectangle;
   }
 
-  const Rectangle &get_rectangle() const{
-    return rectangle;
-  }
+  const Rectangle &get_rectangle() const { return rectangle; }
 
   void append_right(const ObjectsPerRectangle &other) {
     std::vector<std::shared_ptr<Object>> new_objects;
@@ -137,7 +153,8 @@ struct ObjectsPerRectangle {
       }
     }
     // merge all objects
-    std::vector<std::shared_ptr<Object>> other_objects_touching_up = other.objects_touching_up;
+    std::vector<std::shared_ptr<Object>> other_objects_touching_up =
+        other.objects_touching_up;
     for (auto &object : objects_touching_down) {
       std::vector<size_t> indexes_to_remove;
       size_t i = 0;
@@ -185,6 +202,66 @@ struct ObjectsPerRectangle {
   }
 
 private:
+  void
+  append(std::function<const std::vector<std::shared_ptr<Object>> &(
+             const ObjectsPerRectangle &)>
+             get_objects,
+         std::function<const std::vector<std::shared_ptr<Object>> &(
+             const ObjectsPerRectangle &)>
+             get_touching_objects_this,
+         std::function<const std::vector<std::shared_ptr<Object>> &(
+             const ObjectsPerRectangle &)>
+             get_touching_objects_other,
+         std::function<void(Rectangle &, const Rectangle &)> merge_rectangles,
+         const ObjectsPerRectangle &other) {
+    std::vector<std::shared_ptr<Object>> new_objects;
+    // first add all objects that are not touching the right side
+    const auto touching_objects_this = get_touching_objects_this(*this);
+    for (auto object : get_objects(*this)) {
+      if (std::find(touching_objects_this.begin(), touching_objects_this.end(),
+                    object) == touching_objects_this.end()) {
+        new_objects.push_back(object);
+      }
+    }
+    // next add all objects from other that are not touching the left side
+    auto touching_objects_other = get_touching_objects_other(other);
+    for (auto object : get_objects(other)) {
+      if (std::find(touching_objects_other.begin(),
+                    touching_objects_other.end(),
+                    object) == touching_objects_other.end()) {
+        new_objects.push_back(object);
+      }
+    }
+    // merge all objects
+    for (auto &object : touching_objects_this) {
+      std::vector<size_t> indexes_to_remove;
+      size_t i = 0;
+      for (auto other_object : touching_objects_other) {
+        const auto merged = object->try_merge_right(*other_object);
+        if (merged) {
+          indexes_to_remove.push_back(i);
+        }
+        i++;
+      }
+      for (auto index : indexes_to_remove) {
+        touching_objects_other.erase(touching_objects_other.begin() + index);
+      }
+      new_objects.push_back(object);
+    }
+    for (auto object : touching_objects_other) {
+      new_objects.push_back(object);
+    }
+    objects.clear();
+    objects_touching_down.clear();
+    objects_touching_up.clear();
+    objects_touching_left.clear();
+    objects_touching_right.clear();
+    merge_rectangles(rectangle, other.rectangle);
+    for (const auto &object : new_objects) {
+      insert_object(object);
+    }
+  }
+
   std::vector<std::shared_ptr<Object>> objects;
   Rectangle rectangle;
   std::vector<std::shared_ptr<Object>> objects_touching_right;
