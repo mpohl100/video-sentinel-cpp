4b0c9939c1805d25b23e20b8183561b179c82a18
Michael Pohl <mpohl100@googlemail.com>
2024-03-22 16:34:25 +0100

    copied make scripts

---
+++ scripts/make_release_clang.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Set default build directory
+BUILD_DIR=$(pwd)/../build
+
+# Check if a build directory is provided as a command line argument
+if [ "$#" -gt 0 ]; then
+    BUILD_DIR="$1"
+fi
+
+# Create the build directory if it doesn't exist
+mkdir -p "$BUILD_DIR"
+
+cd "$BUILD_DIR"
+
+conan build .. -b="missing" -pr:b="$(pwd)/../conan_profiles/clang14_cpp20.prof" -s build_type=Release -c tools.system.package_manager:mode=install -c tools.system.package_manager:sudo=True 
