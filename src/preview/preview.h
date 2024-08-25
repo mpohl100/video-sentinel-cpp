ffa388eef6b511f0e07ae3d4aa764d9a0a40a69e
Michael Pohl <mpohl100@googlemail.com>
2024-08-25 19:04:38 +0000

    mutex retrieval of rectangles. Provide opportunity to query whether rectangles are already requested

--- src/preview/preview.h
+++ src/preview/preview.h
@@ -4,13 +4,28 @@
 #include "par/parallel.h"
 #include "webcam/webcam.h"
 
+#include <iostream>
+#include <mutex>
 #include <optional>
 #include <vector>
-#include <iostream>
+#include <string>
 
 namespace preview {
 
-enum FrameCalculationStatus { NOT_STARTED, IN_PROGRESS, DONE };
+enum class FrameCalculationStatus { NOT_STARTED, IN_PROGRESS, DONE };
+enum class RectanglesQueryStatus { NOT_REQUESTED, REQUESTED };
+
+inline std::string to_string(FrameCalculationStatus status) {
+  switch (status) {
+  case FrameCalculationStatus::NOT_STARTED:
+    return "NOT_STARTED";
+  case FrameCalculationStatus::IN_PROGRESS:
+    return "IN_PROGRESS";
+  case FrameCalculationStatus::DONE:
+    return "DONE";
+  }
+  return "UNKNOWN";
+}
 
 struct VideoPreview {
   VideoPreview() = default;
@@ -31,6 +46,7 @@ struct VideoPreview {
     bool is_done =
         _executor.wait_for(_current_task, std::chrono::microseconds(0));
     if (is_done) {
+      std::unique_lock<std::mutex> lock(_processed_mutex);
       _processed_frame_data = std::move(_current_frame_data);
       _current_original.release();
       _frame_calculation_status = FrameCalculationStatus::DONE;
@@ -41,18 +57,25 @@ struct VideoPreview {
     return _frame_calculation_status;
   }
 
-  std::vector<od::Rectangle> get_all_rectangles() const {
+  RectanglesQueryStatus get_rectangles_query_status() {
+    return _rectangles_query_status;
+  }
+
+  std::vector<od::Rectangle> get_all_rectangles() {
+    std::unique_lock<std::mutex> lock(_processed_mutex);
+    _rectangles_query_status = RectanglesQueryStatus::REQUESTED;
     return _processed_frame_data.all_rectangles.rectangles;
   }
 
   void set_mat(cv::Mat const &mat, od::Rectangle const &rectangle, int rings,
                int gradient_threshold) {
-    std::cout << "setting mat" << std::endl;
     _frame_calculation_status = FrameCalculationStatus::IN_PROGRESS;
-    _current_original = mat.clone();	
+    _rectangles_query_status = RectanglesQueryStatus::NOT_REQUESTED;
+    _current_original = mat.clone();
     _current_frame_data = webcam::FrameData{_current_original};
-    _current_task = webcam::process_frame(_current_frame_data, _current_original,
-                                          rectangle, rings, gradient_threshold);
+    _current_task =
+        webcam::process_frame(_current_frame_data, _current_original, rectangle,
+                              rings, gradient_threshold);
     _executor.run(_current_task);
   }
 
@@ -64,6 +87,9 @@ private:
   par::Task _current_task;
   FrameCalculationStatus _frame_calculation_status =
       FrameCalculationStatus::NOT_STARTED;
+  RectanglesQueryStatus _rectangles_query_status =
+      RectanglesQueryStatus::NOT_REQUESTED;
+  std::mutex _processed_mutex;
 };
 
 } // namespace preview
