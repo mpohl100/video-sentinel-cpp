4b7e1617f60f581c9a0100ed4fa2a92bf33e4740
Michael Pohl <mpohl100@googlemail.com>
2024-09-09 15:20:44 +0200

    added further tests for deduce objects

--- tests/slices.cpp
+++ tests/slices.cpp
@@ -10,6 +10,104 @@
 namespace {
 
 TEST_CASE("Slices", "[slices]") {
+  SECTION("SlicesDetectOneSimpleObject"){
+    // Arrange
+    auto slices = od::Slices{math2d::Point{0, 0}};
+
+    // construct first line
+    auto slice = od::AnnotatedSlice{od::Slice{math2d::Point{0, 0}, math2d::Point{10, 0}}, 0};
+    auto slice_line = od::SliceLine{std::vector<od::AnnotatedSlice>{slice}, 0};
+    slices.slices.push_back(slice_line);
+
+    // Act
+    auto objects = od::deduce_objects(slices);
+
+    // Assert
+    CHECK(objects.size() == 1);
+  }
+
+  
+  SECTION("SlicesDetectTwoSimpleObjects"){
+    // Arrange
+    auto slices = od::Slices{math2d::Point{0, 0}};
+
+    // construct first line
+    auto slice = od::AnnotatedSlice{od::Slice{math2d::Point{0, 0}, math2d::Point{10, 0}}, 0};
+    auto neighbour_slice = od::AnnotatedSlice{od::Slice{math2d::Point{21, 0}, math2d::Point{30, 0}}, 0};
+    auto slice_line = od::SliceLine{std::vector<od::AnnotatedSlice>{slice, neighbour_slice}, 0};
+    slices.slices.push_back(slice_line);
+
+    // Act
+    auto objects = od::deduce_objects(slices);
+
+    // Assert
+    CHECK(objects.size() == 2);
+  }
+
+  SECTION("SlicesDetectswoObjectOnTwoDifferentLines"){
+    // Arrange
+    auto slices = od::Slices{math2d::Point{0, 0}};
+
+    // construct first line
+    auto slice = od::AnnotatedSlice{od::Slice{math2d::Point{0, 0}, math2d::Point{10, 0}}, 0};
+    auto neighbour_slice = od::AnnotatedSlice{od::Slice{math2d::Point{21, 0}, math2d::Point{30, 0}}, 0};
+    auto slice_line = od::SliceLine{std::vector<od::AnnotatedSlice>{slice, neighbour_slice}, 0};
+    slices.slices.push_back(slice_line);
+
+    // construct second line
+    auto second_slice = od::AnnotatedSlice{od::Slice{math2d::Point{0, 1}, math2d::Point{10, 1}}, 1};
+    auto second_neighbour_slice = od::AnnotatedSlice{od::Slice{math2d::Point{21, 1}, math2d::Point{30, 1}}, 1};
+    auto second_slice_line = od::SliceLine{std::vector<od::AnnotatedSlice>{second_slice, second_neighbour_slice}, 1};
+    slices.slices.push_back(second_slice_line);
+
+    // Act
+    auto objects = od::deduce_objects(slices);
+
+    // Assert
+    CHECK(objects.size() == 2);
+  }
+
+  SECTION("SlicesDetectsOneObjectDiagonallyDown"){
+    // Arrange
+    auto slices = od::Slices{math2d::Point{0, 0}};
+
+    // construct first line
+    auto slice = od::AnnotatedSlice{od::Slice{math2d::Point{0, 0}, math2d::Point{10, 0}}, 0};
+    auto slice_line = od::SliceLine{std::vector<od::AnnotatedSlice>{slice}, 0};
+    slices.slices.push_back(slice_line);
+
+    // construct second line
+    auto second_slice = od::AnnotatedSlice{od::Slice{math2d::Point{11, 1}, math2d::Point{20, 1}}, 1};
+    auto second_slice_line = od::SliceLine{std::vector<od::AnnotatedSlice>{second_slice}, 1};
+    slices.slices.push_back(second_slice_line);
+
+    // Act
+    auto objects = od::deduce_objects(slices);
+
+    // Assert
+    CHECK(objects.size() == 1);
+  }
+
+  SECTION("SlicesDetectsOneObjectDiagonallyUp"){
+    // Arrange
+    auto slices = od::Slices{math2d::Point{0, 0}};
+
+    // construct first line
+    auto slice = od::AnnotatedSlice{od::Slice{math2d::Point{11, 0}, math2d::Point{20, 0}}, 0};
+    auto slice_line = od::SliceLine{std::vector<od::AnnotatedSlice>{slice}, 0};
+    slices.slices.push_back(slice_line);
+
+    // construct second line
+    auto second_slice = od::AnnotatedSlice{od::Slice{math2d::Point{0, 1}, math2d::Point{10, 1}}, 1};
+    auto second_slice_line = od::SliceLine{std::vector<od::AnnotatedSlice>{second_slice}, 1};
+    slices.slices.push_back(second_slice_line);
+
+    // Act
+    auto objects = od::deduce_objects(slices);
+
+    // Assert
+    CHECK(objects.size() == 1);
+  }
 
   SECTION("SlicesCreatesSnakeAsOneObject") {
     // Arrange
