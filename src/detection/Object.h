b68264394a9770045e3a1d009a5625ea88897bbc
Michael Pohl <mpohl100@googlemail.com>
2024-10-20 15:18:25 +0200

    pimpled object

--- src/detection/Object.h
+++ src/detection/Object.h
@@ -14,17 +14,17 @@
 
 namespace od {
 
-struct Object {
-  Object() = default;
-  Object(const Object &) = default;
-  Object(Object &&) = default;
-  Object &operator=(const Object &) = default;
-  Object &operator=(Object &&) = default;
-  Object(const Slices &slices) : slices{slices} {}
+struct ObjectImpl {
+  ObjectImpl() = default;
+  ObjectImpl(const ObjectImpl &) = default;
+  ObjectImpl(ObjectImpl &&) = default;
+  ObjectImpl &operator=(const ObjectImpl &) = default;
+  ObjectImpl &operator=(ObjectImpl &&) = default;
+  ObjectImpl(const Slices &slices) : slices{slices} {}
 
   std::string to_string() const { return slices.to_string(); }
 
-  bool try_merge_right(Object &other) {
+  bool try_merge_right(ObjectImpl &other) {
     if (slices.touching_right(other.slices)) {
       slices.merge_right(other.slices);
       return true;
@@ -32,7 +32,7 @@ struct Object {
     return false;
   }
 
-  bool try_merge_down(Object &other, bool debug = false) {
+  bool try_merge_down(ObjectImpl &other, bool debug = false) {
     if (debug) {
       std::cout << "try_merge_down" << std::endl;
       std::cout << "this: " << slices.to_string() << std::endl;
@@ -51,11 +51,11 @@ struct Object {
     return false;
   }
 
-  bool touching_right(Object &other) {
+  bool touching_right(ObjectImpl &other) {
     return slices.touching_right(other.slices);
   }
 
-  bool touching_down(Object &other) {
+  bool touching_down(ObjectImpl &other) {
     return slices.touching_down(other.slices);
   }
 
@@ -66,6 +66,34 @@ struct Object {
   }
 
   Slices slices;
+};
+
+struct Object{
+    std::string to_string() const { return object->to_string(); }
+
+  bool try_merge_right(Object other) {
+    object->try_merge_right(*other.object);
+  }
+
+  bool try_merge_down(Object other, bool debug = false) {
+    object->try_merge_down(*other.object, debug);
+  }
+
+  bool touching_right(Object other) {
+    return object->touching_right(*other.object);
+  }
+
+  bool touching_down(Object other) {
+    return object->touching_down(*other.object);
+  }
+
+  od::Rectangle get_bounding_box() const { return object->get_bounding_box(); }
+
+  bool contains_point(const math2d::Point &point) const {
+    return object->contains_point(point);
+  }
+private:
+  std::shared_ptr<od::ObjectImpl> object;
 };
 
 } // namespace od
