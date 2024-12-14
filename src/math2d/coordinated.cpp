db4781a3e75e23b334c7367723b0f6cb9a02fbf4
Michael Pohl <mpohl100@googlemail.com>
2024-12-14 17:27:32 +0000

    new implementation of coordinated

--- src/math2d/coordinated.cpp
+++ src/math2d/coordinated.cpp
@@ -1,82 +1,130 @@
 #include "coordinated.h"
+#include <cmath>
+#include <sstream>
+#include <stdexcept>
 
-namespace math2d {
+namespace math2d{
 
+// Helper constants and functions
+constexpr double PI = 3.141592653589793;
+constexpr double to_radians(double degrees) { return degrees * PI / 180.0; }
+constexpr double to_degrees(double radians) { return radians * 180.0 / PI; }
+
+// CoordinateSystem Constructor
 CoordinateSystem::CoordinateSystem(Point origin, Vector x_axis, Vector y_axis)
-    : origin{origin}, x_axis{x_axis}, y_axis{y_axis} {}
+    : origin(origin), x_axis(x_axis), y_axis(y_axis) {}
 
+// Convert from CoordinatedPoint to Euclidean Point
 Point CoordinateSystem::to_euclidian(const CoordinatedPoint &point) const {
-  const auto x = point.x * x_axis.x + point.y * y_axis.x + origin.x;
-  const auto y = point.x * x_axis.y + point.y * y_axis.y + origin.y;
-  return Point{x, y};
+  return Point{
+      origin.x + point.x * x_axis.x + point.y * y_axis.x,
+      origin.y + point.x * x_axis.y + point.y * y_axis.y};
 }
 
+// Convert from Euclidean Point to CoordinatedPoint
 CoordinatedPoint CoordinateSystem::from_euclidian(const Point &point) const {
-  const auto x =
-      (point.x - origin.x) * x_axis.x + (point.y - origin.y) * x_axis.y;
-  const auto y =
-      (point.x - origin.x) * y_axis.x + (point.y - origin.y) * y_axis.y;
-  return CoordinatedPoint{x, y, *this};
+  Vector displacement{point.x - origin.x, point.y - origin.y};
+
+  // Solve for x and y using dot products
+  double det = x_axis.x * y_axis.y - x_axis.y * y_axis.x;
+  if (std::abs(det) < 1e-9)
+    throw std::runtime_error("Axes are linearly dependent or degenerate");
+
+  double xx = (displacement.x * y_axis.y - displacement.y * y_axis.x) / det;
+  double yy = (displacement.y * x_axis.x - displacement.x * x_axis.y) / det;
+
+  return CoordinatedPoint(xx, yy, *this);
 }
 
+// Rotate the CoordinateSystem around its origin by a given angle
 void CoordinateSystem::rotate(const Angle &angle) {
-  const auto x_axis_rotated = x_axis.rotate(angle);
-  const auto y_axis_rotated = y_axis.rotate(angle);
-  x_axis = x_axis_rotated;
-  y_axis = y_axis_rotated;
+  double cos_a = std::cos(angle.radians());
+  double sin_a = std::sin(angle.radians());
+
+  x_axis = Vector{x_axis.x * cos_a - x_axis.y * sin_a,
+                  x_axis.x * sin_a + x_axis.y * cos_a};
+  y_axis = Vector{y_axis.x * cos_a - y_axis.y * sin_a,
+                  y_axis.x * sin_a + y_axis.y * cos_a};
 }
 
+// Convert CoordinateSystem to string
 std::string CoordinateSystem::to_string() const {
-  return std::string("CoordinateSystem(origin: ") + origin.toString() +
-         "; x_axis: " + x_axis.toString() + "; y_axis: " + y_axis.toString() +
-         ")";
+  std::ostringstream oss;
+  oss << "Origin: (" << origin.x << ", " << origin.y << ")\n"
+      << "X-axis: (" << x_axis.x << ", " << x_axis.y << ")\n"
+      << "Y-axis: (" << y_axis.x << ", " << y_axis.y << ")";
+  return oss.str();
 }
 
-CoordinatedPoint CoordinatedPoint::convert_to(
-    const CoordinateSystem &new_coordinate_system) const {
-  const auto euclidian_coords = coordinate_system.to_euclidian(*this);
-  return new_coordinate_system.from_euclidian(euclidian_coords);
+// Convert CoordinatedPoint to a new CoordinateSystem
+CoordinatedPoint CoordinatedPoint::convert_to(const CoordinateSystem &new_coordinate_system) const {
+  // Convert current point to Euclidean
+  Point euclidean_point = coordinate_system.to_euclidian(*this);
+
+  // Convert Euclidean point to the new coordinate system
+  return new_coordinate_system.from_euclidian(euclidean_point);
 }
 
-/// @brief This function adds a vector to a point. It assumes that the vector is
-/// in the same coordinate system as the point.
-/// @param vec the vector to add to the point
-/// @return the coordinate point that is the result of adding the vector to the
-/// original point in this
+// Add a vector to the point
 CoordinatedPoint CoordinatedPoint::plus(const Vector &vec) const {
-  const auto x = this->x + vec.x;
-  const auto y = this->y + vec.y;
-  return CoordinatedPoint{x, y, this->coordinate_system};
+  return CoordinatedPoint{x + vec.x, y + vec.y, coordinate_system};
 }
 
+// Convert CoordinatedPoint to string
 std::string CoordinatedPoint::toString() const {
-  return std::string("CoordinatedPoint(") + std::to_string(x) + ", " +
-         std::to_string(y) + ")";
+  std::ostringstream oss;
+  oss << "(" << x << ", " << y << ") in \n" << coordinate_system.to_string();
+  return oss.str();
 }
 
-CoordinatedPoint CoordinatedPoint::rotate(const CoordinatedPoint &around,
-                                          const Angle &angle) const {
-  const auto this_point_euclidian = coordinate_system.to_euclidian(*this);
-  const auto around_point_euclidian =
-      around.coordinate_system.to_euclidian(around);
-  const auto rotated_point_euclidian =
-      this_point_euclidian.rotate(around_point_euclidian, angle);
-  return coordinate_system.from_euclidian(rotated_point_euclidian);
+// Rotate CoordinatedPoint around another CoordinatedPoint by a given angle
+CoordinatedPoint CoordinatedPoint::rotate(const CoordinatedPoint &around_point,
+                                           const Angle &angle) const {
+  const auto around = around_point.convert_to(coordinate_system);
+
+  double cos_a = std::cos(angle.radians());
+  double sin_a = std::sin(angle.radians());
+
+  // Translate the point to the origin of rotation
+  double dx = x - around.x;
+  double dy = y - around.y;
+
+  // Rotate
+  double new_x = dx * cos_a - dy * sin_a;
+  double new_y = dx * sin_a + dy * cos_a;
+
+  // Translate back
+  return CoordinatedPoint(new_x + around.x, new_y + around.y, coordinate_system);
 }
 
-number_type CoordinatedPoint::distance_to(const CoordinatedPoint &point) const {
-  const auto this_point_euclidian = coordinate_system.to_euclidian(*this);
-  const auto other_point_euclidian =
-      point.coordinate_system.to_euclidian(point);
-  return Vector{this_point_euclidian, other_point_euclidian}.magnitude();
+// Distance between two points
+number_type CoordinatedPoint::distance_to(const CoordinatedPoint &other_point) const {
+  const auto point = other_point.convert_to(coordinate_system);
+
+  double dx = x - point.x;
+  double dy = y - point.y;
+  return std::sqrt(dx * dx + dy * dy);
 }
 
+// Get the Euclidean Point corresponding to this CoordinatedPoint
 Point CoordinatedPoint::get_point() const {
   return coordinate_system.to_euclidian(*this);
 }
 
-bool operator<(const CoordinatedPoint &l, const CoordinatedPoint &r);
-bool operator==(const CoordinatedPoint &l, const CoordinatedPoint &r);
-bool operator!=(const CoordinatedPoint &l, const CoordinatedPoint &r);
+// Operators for comparison
+bool operator<(const CoordinatedPoint &l, const CoordinatedPoint &r_cmp) {
+  const auto r = r_cmp.convert_to(l.coordinate_system);
+  return (l.x < r.x) || (l.x == r.x && l.y < r.y);
+}
+
+bool operator==(const CoordinatedPoint &l, const CoordinatedPoint &r_cmp) {
+  const auto r = r_cmp.convert_to(l.coordinate_system);  
+  constexpr auto epsilon = 1e-9;
+  return std::abs(l.x - r.x) < epsilon && std::abs(l.y - r.y) < epsilon;
+}
+
+bool operator!=(const CoordinatedPoint &l, const CoordinatedPoint &r) {
+  return !(l == r);
+}
 
 } // namespace math2d
